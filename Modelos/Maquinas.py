2c8b4acc65270133a5de15a83fc26e28b0afae29
YagoMartins77 <yagofelipeoliveira3@gmail.com>
2025-11-12 21:43:47 -0300

    Update Maquinas.py

---
+++ Modelos/Maquinas.py
@@ -0,0 +1,36 @@
+class Maquina:
+    def_init_(self, nome_mqn, id_mqn, parte_trabalhada):
+        self._nome_mqn = nome_mqn
+        self._id_mqn = id_mqn
+        self._parte_trabalhada = parte_trabalhada
+        
+
+    @property
+    def nome_mqn(self):
+        return self._nome_mqn
+
+    @nome_mqn.setter
+    def nome_mqn(self, value):
+        self._nome_mqn = value
+
+    @property
+    def id_mqn(self):
+        return self._id_mqn
+
+    @id_mqn.setter
+    def id_mqn(self, value):
+        self._id_mqn = value
+
+    @property
+    def parte_trabalhada(self):
+        return self._parte_trabalhada
+
+    @parte_trabalhada.setter
+    def parte_trabalhada(self, value):
+        self._parte_trabalhada = value
+
+        pass
+
+    
+
+
